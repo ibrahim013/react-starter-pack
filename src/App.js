349ed9783602427c30cd9f823fc6344e215dbc75
Ibrahim Abdulazeez <waleibrahim13@gmail.com>
2020-06-16 19:06:19 +0100

    small changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <>
-      <div> Welcome, project setup sucessfull</div>
+      <div> Welcome, project setup sucessfull, This should work for all</div>
     </>
   );
 }
